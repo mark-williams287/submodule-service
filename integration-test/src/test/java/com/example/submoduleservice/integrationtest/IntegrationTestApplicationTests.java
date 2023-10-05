daf953bae56ab91e5bab1281fd912cc940301570
Mark Williams <mark.williams@currencycloud.com>
2023-10-05 10:38:33 +0100

    log test

--- integration-test/src/test/java/com/example/submoduleservice/integrationtest/IntegrationTestApplicationTests.java
+++ integration-test/src/test/java/com/example/submoduleservice/integrationtest/IntegrationTestApplicationTests.java
@@ -1,12 +1,15 @@
 package com.example.submoduleservice.integrationtest;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class IntegrationTestApplicationTests {
 
+    private static final Logger logger = LoggerFactory.getLogger(IntegrationTestApplicationTests.class);
+
     @Test
     void testRuns() {
-        Assertions.assertTrue(true);
+        logger.info("Running test in service");
     }
 }
