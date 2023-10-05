72196e564267ff3cc85c8487b957698e7386885c
Mark Williams <mark.williams@currencycloud.com>
2023-10-05 10:31:11 +0100

    initial commit

---
+++ integration-test/src/test/java/com/example/submoduleservice/integrationtest/IntegrationTestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.submoduleservice.integrationtest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class IntegrationTestApplicationTests {
+
+    @Test
+    void testRuns() {
+        Assertions.assertTrue(true);
+    }
+}
---
+++ src/main/java/com/example/submoduleservice/SubmoduleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.submoduleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmoduleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmoduleServiceApplication.class, args);
+    }
+
+}
